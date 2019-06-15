b3f692b5d12df1634e56bfc2e6c0cefe78cb84c4
yuekui <yuekui001@ke.com>
2019-06-15 21:39:16 +0800

    add

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+/** 
+ * 1.删除了分支 还想找到该分支
+ * 2.
+*/
 function a(){
   let a =1;
   let b=2;
@@ -5,4 +9,10 @@ function a(){
   let d=4
   let e=2222222
   console.log(a+b+c+d+e)
+}
+
+function b(){
+  let a=1;
+  let b=2;
+  console.log(a+b)
 }
