0fba6626e93b1f51d99e50ea0d9666af66f50803
yuekui <yuekui001@ke.com>
2019-06-15 23:03:20 +0800

    test revert

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
-/** 
- * 1.删除了分支 还想找到该分支
- * 2.
-*/
 function a(){
   let a =1;
   let b=2;
@@ -9,11 +5,4 @@ function a(){
   let d=4
   let e=2222222
   console.log(a+b+c+d+e)
-}
-
-function b(){
-  let a=1;
-  let b=2;
-  let c=0;
-  console.log(a+b)
 }
