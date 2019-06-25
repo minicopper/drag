09015590b25f788d802b7a661ae7232da330d96e
yuekui <yuekui001@ke.com>
2019-06-25 15:14:40 +0800

    test3

--- drag2.js
+++ drag2.js
@@ -2,6 +2,7 @@
  * Created by Administrator on 2017/9/2 0002.
  */
 function test(){
-  console.log('t1111e222st2221111')
-  console.log(2222)
+  console.log('2222')
+  console.log(22333)
+  console.log(2223333)
 }
